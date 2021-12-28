05a8f40a867ee72c19c7beb1102ea2a4441b8c4e
vigneshkanna <vkrishitaran@gmail.com>
2021-12-28 20:12:41 +0530

    app icon added

--- photo-sharing-app/App.js
+++ photo-sharing-app/App.js
@@ -3,6 +3,10 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import Header from "./components/header";
 import ImageShare from "./components/imageShare";
 import Credit from "./components/credits";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 5000);
 // Main App
 export default function App() {
   return (
