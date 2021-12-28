350ea42aa983f70c970ac3fd4524c316636668dd
vigneshkanna <vkrishitaran@gmail.com>
2021-12-28 20:07:44 +0530

    image sharing code added

--- photo-sharing-app/components/imageShare.js
+++ photo-sharing-app/components/imageShare.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as Sharing from "expo-sharing";
 import sampleImage from "../assets/vk-round-logo.png";
@@ -8,8 +9,12 @@ import sampleImage from "../assets/vk-round-logo.png";
 
 // Image sharing component
 export default function ImageShare() {
-  // Image picking logi (Async)
+  // useState logic to set the picked image
+  let [pickedImage, setPickedImage] = React.useState(null);
+
+  // Image picking logic (Async)
   let openImagePickerAsync = async () => {
+    // getting image access permission
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     if (permissionResult.granted === false) {
@@ -17,9 +22,42 @@ export default function ImageShare() {
       return;
     }
 
+    // If permission granted then pick image from gallery
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
+    setPickedImage({ localUri: pickerResult.uri });
   };
+
+  let ImageShareCompatibility = async () => {
+    // checking the device sharing compatibility
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh No, sharing isn't available on your platform 😡`);
+      return;
+    }
+    // If compatibile then ask user the mode of sharing
+    await Sharing.shareAsync(pickedImage.localUri);
+  };
+
+  // Image sharing mode selection component
+  if (pickedImage !== null) {
+    return (
+      <View style={styles.share_container}>
+        <Image source={{ uri: pickedImage.localUri }} style={styles.thumbnail} />
+        <TouchableOpacity onPress={ImageShareCompatibility} style={styles.share_btn}>
+          <Text style={{ fontSize: 18, fontFamily: "Roboto", color: "#F2F3E8", fontWeight: "bold" }}>
+            Share this photo
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={openImagePickerAsync} style={styles.share_btn}>
+          <Text style={{ fontSize: 18, fontFamily: "Roboto", color: "#F2F3E8", fontWeight: "bold" }}>
+            Dont like this Image
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  // Image sharing home component
+  // This instructs to pick a image from gallery
   return (
     <View style={styles.share_container}>
       <Image source={sampleImage} style={styles.sample_image} />
@@ -62,7 +100,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#9D2DD2",
     borderRadius: 10,
     padding: 20,
-    borderBottomColor: "white",
     marginTop: 30,
+  },
+  share_btn: {
+    backgroundColor: "red",
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 10,
+    width: 240,
+    alignItems: "center",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    alignItems: "center",
+    borderRadius: 10,
+    resizeMode: "contain",
+    marginBottom: 20,
   },
 });
